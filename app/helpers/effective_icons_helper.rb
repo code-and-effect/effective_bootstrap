4c53aa2df0046dbf1af3eac814cb48468cd11ab2
Matt Riemer <matthew@agilestyle.com>
2018-02-15 10:58:49 -0700

    icons

--- app/helpers/effective_icons_helper.rb
+++ app/helpers/effective_icons_helper.rb
@@ -3,7 +3,7 @@ module EffectiveIconsHelper
   def icon(svg, options = {})
     svg = svg.to_s.chomp('.svg')
 
-    options.reverse_merge!(nocomment: true, preserve_aspect_ratio: true)
+    options.reverse_merge!(nocomment: true)
     options[:class] = [options[:class], "eb-icon eb-icon-#{svg}"].compact.join(' ')
 
     inline_svg(svg + '.svg', options)
