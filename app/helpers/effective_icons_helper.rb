fb29d097a1d70d75d1b9108f2ca3ce39cb730fb1
Matt Riemer <matthew@agilestyle.com>
2018-02-16 17:00:07 -0700

    icon helpers

--- app/helpers/effective_icons_helper.rb
+++ app/helpers/effective_icons_helper.rb
@@ -13,5 +13,34 @@ module EffectiveIconsHelper
     link_to(icon(svg), url, options)
   end
 
+  def show_icon_to(path, options = {})
+    icon_to('eye-open', path, { title: 'Show' }.merge(options))
+  end
+
+  def edit_icon_to(path, options = {})
+    icon_to('edit', path, { title: 'Edit' }.merge(options))
+  end
+
+  def destroy_icon_to(path, options = {})
+    defaults = { title: 'Destroy', data: { method: :delete, confirm: 'Delete this item?' } }
+    icon_to('trash', path, defaults.merge(options))
+  end
+
+  def settings_icon_to(path, options = {})
+    icon_to('cog', path, { title: 'Settings' }.merge(options))
+  end
+
+  def ok_icon_to(path, options = {})
+    icon_to('ok', path, { title: 'OK' }.merge(options))
+  end
+
+  def approve_icon_to(path, options = {})
+    icon_to('ok', path, { title: 'Approve' }.merge(options))
+  end
+
+  def remove_icon_to(path, options = {})
+    icon_to('remove', path, { title: 'Remove' }.merge(options))
+  end
+
 end
 
