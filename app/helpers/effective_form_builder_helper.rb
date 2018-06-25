d4de4be8cf40eb1a72961e84eb461c043bf88970
Matt Riemer <matthew@agilestyle.com>
2018-06-25 10:50:23 -0600

    Add default form ids

--- app/helpers/effective_form_builder_helper.rb
+++ app/helpers/effective_form_builder_helper.rb
@@ -3,6 +3,19 @@ module EffectiveFormBuilderHelper
     options[:class] = [options[:class], 'needs-validation', ('form-inline' if options[:layout] == :inline)].compact.join(' ')
     options[:html] = (options[:html] || {}).merge(novalidate: true, onsubmit: 'return EffectiveForm.validate(this);')
 
+    # Compute the default ID
+    subject = Array(options[:scope] || options[:model]).last
+
+    html_id = if subject.kind_of?(Symbol)
+      subject
+    elsif subject.respond_to?(:new_record?) && subject.new_record?
+      "new_#{subject.class.name.underscore}"
+    elsif subject.respond_to?(:persisted?) && subject.persisted?
+      "edit_#{subject.class.name.underscore}_#{subject.to_param}"
+    else
+      raise 'Unexpected subject. Expected :scope or :model to be a symbol or ActiveRecord object'
+    end
+
     remote_index = options.except(:model).hash.abs
 
     if options.delete(:remote) == true
@@ -15,7 +28,12 @@ module EffectiveFormBuilderHelper
         options[:html]['data-remote'] = true
         options[:html]['data-remote-index'] = remote_index
       end
+
+      html_id = "#{html_id}_#{remote_index}"
     end
+
+    # Assign default ID
+    options[:id] ||= options[:html].delete(:id) || html_id
 
     without_error_proc do
       form_with(**options.merge(builder: Effective::FormBuilder), &block)
