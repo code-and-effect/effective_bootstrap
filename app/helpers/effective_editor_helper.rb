d4864378fa18691c7247ba5d72ed816faca038e3
Matt Riemer <matthew@agilestyle.com>
2018-06-26 15:54:52 -0600

    effective_editor_tag

--- app/helpers/effective_editor_helper.rb
+++ app/helpers/effective_editor_helper.rb
@@ -3,7 +3,7 @@ module EffectiveEditorHelper
   def effective_editor_tag(delta)
     input_js = { method_name: 'effective_editor_tag', theme: 'snow', readOnly: true, modules: { toolbar: false } }
 
-    content_tag(:div, '', id: "ql-#{delta.object_id}", class: 'effective_editor_content ql-effective', data: { 'input-js-options': input_js, 'delta': (delta.presence || '{}').html_safe})
+    content_tag(:div, '', id: "ql-#{delta.object_id}", class: 'effective_editor_content ql-effective', data: { 'input-js-options': input_js, 'delta': delta.presence || '{}'})
   end
 
 end
