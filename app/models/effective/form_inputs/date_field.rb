5a3b70f8a2a251e19f736640f3f43807e2b7bc20
Matt Riemer <matthew@codeandeffect.com>
2018-08-28 12:23:19 -0600

    refactor

--- app/models/effective/form_inputs/date_field.rb
+++ app/models/effective/form_inputs/date_field.rb
@@ -5,20 +5,28 @@ module Effective
       def input_html_options
         {
           class: ['form-control', 'effective_date_time_picker', 'effective_date', ('not-date-linked' if not_date_linked?)].compact.join(' '),
-          pattern: '\d{4}(-\d{2})?(-\d{2})?'
+          pattern: pattern
         }
       end
 
       def input_js_options
-        { format: 'YYYY-MM-DD', showTodayButton: false, showClear: false, useCurrent: 'hour', disabledDates: disabled_dates.presence }.compact
+        { format: format, showTodayButton: false, showClear: false, useCurrent: 'hour', disabledDates: disabled_dates.presence }.compact
       end
 
       def input_group_options
         { input_group: { class: 'input-group effective_date_time_picker_input_group' }, prepend: content_tag(:span, icon('calendar'), class: 'input-group-text') }
       end
 
-      def datetime_to_s
+      def datetime_to_s # ruby
         value&.strftime('%F')
+      end
+
+      def pattern # html
+        '\d{4}(-\d{2})?(-\d{2})?'
+      end
+
+      def format # moment.js
+        'YYYY-MM-DD'
       end
 
     end
