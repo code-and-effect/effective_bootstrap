e18cf7b831c25224cafd0c5f5a41ae74a379a30a
Matt Riemer <matthew@codeandeffect.com>
2018-09-05 14:17:01 -0600

    limit the image uploads to 1mb files

--- app/assets/javascripts/effective_editor/quill.js
+++ app/assets/javascripts/effective_editor/quill.js
@@ -6788,6 +6788,13 @@ BaseTheme.DEFAULTS = (0, _extend2.default)(true, {}, _theme2.default.DEFAULTS, {
               if (fileInput.files != null && fileInput.files[0] != null) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
+                  // effective_bootstrap
+                  if(e.loaded > 1000000) {
+                    reader.abort();
+                    alert('image file size is too big! Please attach an image less than 1mb.');
+                    return;
+                  }
+
                   var range = _this3.quill.getSelection(true);
                   _this3.quill.updateContents(new _quillDelta2.default().retain(range.index).delete(range.length).insert({ image: e.target.result }), _emitter2.default.sources.USER);
                   _this3.quill.setSelection(range.index + 1, _emitter2.default.sources.SILENT);
