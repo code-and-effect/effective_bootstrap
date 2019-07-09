92da30ac0222af80fbd97a5847511425ff67ed63
Matt Riemer <matthew@codeandeffect.com>
2019-07-09 10:07:21 -0600

    integer field should cast

--- app/models/effective/form_inputs/integer_field.rb
+++ app/models/effective/form_inputs/integer_field.rb
@@ -3,13 +3,16 @@ module Effective
     class IntegerField < Effective::FormInput
 
       def build_input(&block)
-        @builder.super_text_field(name, options[:input])
+        @builder.super_text_field(name, options[:input].merge(value: value_to_i))
       end
 
       def input_html_options
         { class: 'form-control effective_integer', autocomplete: 'off' }
       end
 
+      def value_to_i
+        value.to_i if value
+      end
     end
   end
 end
