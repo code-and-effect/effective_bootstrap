32302c765e20f40b10f46dd4585cd79bc5bafdbf
Matt Riemer <matthew@codeandeffect.com>
2020-05-21 11:17:01 -0600

    avoid inittabs js errors

--- app/assets/javascripts/effective_bootstrap/tabs.js
+++ app/assets/javascripts/effective_bootstrap/tabs.js
@@ -1,16 +1,14 @@
-let initTabs = function() {
-  let $tab_with_error = $(".form-control.is-invalid").first().closest('.tab-pane');
+$(document).ready(function() {
+  var $tab_with_error = $(".form-control.is-invalid").first().closest('.tab-pane');
 
   if ($tab_with_error.length > 0) {
     $(".nav.nav-tabs").find("a[href^='#" + $tab_with_error.attr('id') + "']").tab('show');
   } else if (document.location.hash.length > 0) {
-    let $tab_from_url = $(".nav.nav-tabs").find("a[href^='" + document.location.hash + "']");
+    var $tab_from_url = $(".nav.nav-tabs").find("a[href^='" + document.location.hash + "']");
 
     if ($tab_from_url.length > 0) {
       document.location.hash = ""; // This prevents scrolling to the wrong place in the page
       $tab_from_url.tab("show");
     }
   }
-}
-
-$(document).ready(initTabs);
+});
