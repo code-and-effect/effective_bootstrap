657650ac19093ae2556f0ac14520cf04c1dca36f
Matt Riemer <matthew@codeandeffect.com>
2021-03-05 16:11:58 -0700

    revert maxlength 255 change

--- app/models/effective/form_inputs/text_field.rb
+++ app/models/effective/form_inputs/text_field.rb
@@ -2,10 +2,6 @@ module Effective
   module FormInputs
     class TextField < Effective::FormInput
 
-      def input_html_options
-        { class: 'form-control', maxlength: 255, id: tag_id }
-      end
-
     end
   end
 end
