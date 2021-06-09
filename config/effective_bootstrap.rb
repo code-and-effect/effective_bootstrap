68c21ecf30de06a53e172d1ee349db1a326300b1
Matt Riemer <matthew@codeandeffect.com>
2021-06-09 09:24:21 -0600

    require effective_resources

--- config/effective_bootstrap.rb
+++ config/effective_bootstrap.rb
@@ -1,26 +1,4 @@
 EffectiveBootstrap.setup do |config|
-  # Authorization Method
-  #
-  # This method is called by all controller actions with the appropriate action and resource
-  # If it raises an exception or returns false, an Effective::AccessDenied Error will be raised
-  #
-  # Use via Proc:
-  # Proc.new { |controller, action, resource| authorize!(action, resource) }       # CanCan
-  # Proc.new { |controller, action, resource| can?(action, resource) }             # CanCan with skip_authorization_check
-  # Proc.new { |controller, action, resource| authorize "#{action}?", resource }   # Pundit
-  # Proc.new { |controller, action, resource| current_user.is?(:admin) }           # Custom logic
-  #
-  # Use via Boolean:
-  # config.authorization_method = true  # Always authorized
-  # config.authorization_method = false # Always unauthorized
-  #
-  # Use via Method (probably in your application_controller.rb):
-  # config.authorization_method = :my_authorization_method
-  # def my_authorization_method(resource, action)
-  #   true
-  # end
-  config.authorization_method = Proc.new { |controller, action, resource| authorize!(action, resource) }
-
   # Replaces rails_ujs data-confirm with a custom inline implementation.
   # You will need to recompile assets (or "rm -rf tmp/") if you change this.
   config.use_custom_data_confirm = true
--- lib/effective_bootstrap.rb
+++ lib/effective_bootstrap.rb
@@ -1,33 +1,14 @@
 require 'inline_svg'
+require 'effective_resources'
 require 'effective_bootstrap/engine'
 require 'effective_bootstrap/version'
 
 module EffectiveBootstrap
 
-  # The following are all valid config keys
-  mattr_accessor :authorization_method
-
-  mattr_accessor :use_custom_data_confirm
-
-  def self.setup
-    yield self
+  def self.config_keys
+    [:use_custom_data_confirm]
   end
 
-  def self.authorized?(controller, action, resource)
-    @_exceptions ||= [Effective::AccessDenied, (CanCan::AccessDenied if defined?(CanCan)), (Pundit::NotAuthorizedError if defined?(Pundit))].compact
-
-    return !!authorization_method unless authorization_method.respond_to?(:call)
-    controller = controller.controller if controller.respond_to?(:controller)
-
-    begin
-      !!(controller || self).instance_exec((controller || self), action, resource, &authorization_method)
-    rescue *@_exceptions
-      false
-    end
-  end
-
-  def self.authorize!(controller, action, resource)
-    raise Effective::AccessDenied.new('Access Denied', action, resource) unless authorized?(controller, action, resource)
-  end
+  include EffectiveGem
 
 end
