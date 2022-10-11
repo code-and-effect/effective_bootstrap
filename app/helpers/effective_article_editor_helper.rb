3ae8d3fbf56cfde21a6f5803f7e7171ca3fc60c7
Matt Riemer <matthew@codeandeffect.com>
2022-10-11 15:08:33 -0600

    Better rendering of raw html rich text fields

--- app/helpers/effective_article_editor_helper.rb
+++ app/helpers/effective_article_editor_helper.rb
@@ -5,17 +5,17 @@ module EffectiveArticleEditorHelper
   def render_article_editor_action_text_content(content)
     raise('expected content to be an ActionText content') unless content.kind_of?(ActionText::Content)
 
-    rendered = render_action_text_content(content)
+    rendered = content.to_html
 
     if rendered.include?('effective-article-editor')
       doc = Nokogiri::HTML(rendered)
       doc.search('action-text-attachment').each { |fragment| fragment.remove }
 
       # Filter out <html><body>\n and \n</body></html>
-      rendered = doc.inner_html.to_s[13..-16].html_safe
+      rendered = doc.inner_html.to_s[13..-16]
     end
 
-    rendered
+    rendered.html_safe
   end
 
 end
