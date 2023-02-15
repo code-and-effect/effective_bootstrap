6900817ec14663a135829befdb46db529edca03f
Matt Riemer <matthew@codeandeffect.com>
2023-02-15 09:37:59 -0700

    Use a regex instead of Nokogiri to parse out attachments

--- app/helpers/effective_article_editor_helper.rb
+++ app/helpers/effective_article_editor_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module EffectiveArticleEditorHelper
+  ACTION_TEXT_ATTACHMENTS = Regexp.new("<action-text-attachment.*?<\/action-text-attachment>")
 
   def render_article_editor_action_text_content(content)
     raise('expected content to be an ActionText content') unless content.kind_of?(ActionText::Content)
@@ -8,11 +9,7 @@ module EffectiveArticleEditorHelper
     rendered = content.to_html
 
     if rendered.include?('effective-article-editor')
-      doc = Nokogiri::HTML(rendered)
-      doc.search('action-text-attachment').each { |fragment| fragment.remove }
-
-      # Filter out <html><body>\n and \n</body></html>
-      rendered = doc.inner_html.to_s[13..-16]
+      rendered.gsub!(ACTION_TEXT_ATTACHMENTS, '')
     end
 
     rendered.html_safe
