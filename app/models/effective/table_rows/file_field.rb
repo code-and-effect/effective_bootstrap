601ea1ea3cccffd79d5d236cfd75ac3f7c4d07e3
Matt Riemer <matthew@codeandeffect.com>
2023-05-03 12:41:35 -0600

    Add form field support to table builder

--- app/models/effective/table_rows/file_field.rb
+++ app/models/effective/table_rows/file_field.rb
@@ -5,7 +5,17 @@ module Effective
     class FileField < Effective::TableRow
 
       def content
-        raise('todo')
+        values = Array(value) - [nil, '']
+
+        if values.length > 1
+          values.map { |file| content_tag(:div, link_to_file(file)) }.join.html_safe
+        elsif values.length == 1
+          link_to_file(values.first)
+        end
+      end
+
+      def link_to_file(file)
+        link_to(file.filename, template.url_for(file), target: '_blank')
       end
 
     end
