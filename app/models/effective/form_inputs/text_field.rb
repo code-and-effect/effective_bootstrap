dd6ac4456582a7dcc525654d7a1c86df71a54995
Matt Riemer <matthew@codeandeffect.com>
2024-02-14 09:28:52 -0700

    Add max-length 250 to text fields input

--- app/models/effective/form_inputs/text_field.rb
+++ app/models/effective/form_inputs/text_field.rb
@@ -4,6 +4,10 @@ module Effective
   module FormInputs
     class TextField < Effective::FormInput
 
+      def input_html_options
+        { class: 'form-control', maxlength: 250, id: tag_id }
+      end
+
     end
   end
 end
