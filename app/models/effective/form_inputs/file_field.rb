b8ebb00e220c4e02ccecfe07e746a7bf36297ecc
Matt Riemer <matthew@codeandeffect.com>
2024-09-20 12:52:35 -0600

    Add direct-data-upload-url to force direct upload

--- app/models/effective/form_inputs/file_field.rb
+++ app/models/effective/form_inputs/file_field.rb
@@ -21,6 +21,7 @@ module Effective
           class: 'form-control form-control-file btn-outline-secondary',
           multiple: multiple?,
           direct_upload: true,
+          'data-direct-upload-url': @template.main_app.rails_direct_uploads_url,
           'data-progress-template': progress_template,
           'data-click-submit': (true if click_submit?),
         }.compact
